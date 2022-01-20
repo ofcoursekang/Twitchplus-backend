533e89fde2bac2ba32e2cd461e5c7ca9d970413f
ofcoursekang <alexfeng@berkeley.edu>
2022-01-20 00:29:05 -0800

    authentication update

---
+++ src/main/java/com/Twitchplus/Twitchplus/servlet/RegisterServlet.java
@@ -0,0 +1,42 @@
+package com.Twitchplus.Twitchplus.servlet;
+
+import com.Twitchplus.Twitchplus.db.MySQLConnection;
+import com.Twitchplus.Twitchplus.db.MySQLException;
+import com.Twitchplus.Twitchplus.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RegisterServlet", urlPatterns = {"/register"})
+public class RegisterServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Read user data from the request body
+        ObjectMapper mapper = new ObjectMapper();
+        User user = mapper.readValue(request.getReader(), User.class);
+        if (user == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        boolean isUserAdded = false;
+        MySQLConnection connection = null;
+        try {
+            // Add the new user to the database
+            connection = new MySQLConnection();
+            user.setPassword(ServletUtil.encryptPassword(user.getUserId(), user.getPassword()));
+            isUserAdded = connection.addUser(user);
+        } catch (MySQLException e) {
+            throw new ServletException(e);
+        } finally {
+            connection.close();
+        }
+
+        if (!isUserAdded) {
+            response.setStatus(HttpServletResponse.SC_CONFLICT);
+        }
+    }
+}
